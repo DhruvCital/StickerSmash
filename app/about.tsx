436e9ff721e2ecb0a20a4ffb24f410875d5bf435
Dhruv Bhakt <dhruv@cital.co>
2025-01-29 18:21:23 +0530

    Add navigation and About page link to home screen

--- app/about.tsx
+++ app/about.tsx
@@ -1,11 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
-export default function Index() {
+
+export default function About() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>how it  works - It is OTA On-The-Air</Text>
-      <Link href="/about">
-        <Text style={styles.link}>About me</Text>
+      <Text style={styles.text}>About me!! - I am Dhruv</Text>
+      <Text style={styles.info}>Heder will show back button on iOS with the previous screen title if it doesn't fit then it will show Back label</Text>
+      <Link href="/" asChild>
+        <Text style={styles.link}>Open Index again</Text>
       </Link>
     </View>
   );
@@ -30,5 +32,16 @@ const styles = StyleSheet.create({
     textDecorationLine: "underline",
     marginTop: 10,
   },
+  info: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "normal",
+    marginTop: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#5588ff",
+    borderRadius: 10,
+  },
 });
+
 
