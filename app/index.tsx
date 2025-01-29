df70664f27bade6482dec0c54a9f3c5940c8b463
Dhruv Bhakt <dhruv@cital.co>
2025-01-29 17:29:10 +0530

    Enhance text styling with dark theme and improved readability

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>how it works - It is OTA On-The-Air</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#323232"
+  },
+  text: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
