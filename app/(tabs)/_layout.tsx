e0e96047c6d206a9cff24decf9cfb05ef03125a8
Dhruv Bhakt <dhruv@cital.co>
2025-01-30 11:23:37 +0530

    Implement tab-based navigation with dark theme and status bar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,55 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+
+export default function TabsLayout() {
+  return (
+    <>
+        <StatusBar style="light" />
+        <Tabs
+        screenOptions={{
+            tabBarActiveTintColor: "#ffd33d",
+            headerStyle: {
+            backgroundColor: "#282828",
+            shadowColor: "#fff",
+            },
+            headerShadowVisible: true,
+            headerTintColor: "#fff",
+            tabBarStyle: {
+            backgroundColor: "#282828",
+            borderTopWidth: 0.5,
+            borderTopColor: "rgba(256, 256, 256, 0.2)",
+            }
+        }}
+        >
+        <Tabs.Screen
+            name="index"
+            options={{
+            headerTitle: "Home",
+            headerLeft: () => <></>,
+            tabBarIcon: ({ focused, color }) => (
+                <Ionicons
+                name={focused ? "home-sharp" : "home-outline"}
+                color={color}
+                size={30}
+                />
+            )
+            }}
+        />
+        <Tabs.Screen
+            name="about"
+            options={{
+            headerTitle: "About",
+            tabBarIcon: ({ focused, color }) => (
+                <Ionicons
+                name={focused ? "information-circle-sharp" : "information-circle-outline"}
+                color={color}
+                size={30}
+                />
+            )
+            }}
+        />
+        </Tabs>
+    </>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "StickerSmash",  headerLeft: () => <></> }} />
-      <Stack.Screen name="about" options={{ headerTitle: "About" }} />
-      <Stack.Screen name="+not-found" options={{ }} />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
