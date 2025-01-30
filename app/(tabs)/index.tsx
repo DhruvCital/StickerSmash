1de18ed967ced5f9b8d94879e7687804578cfc14
Dhruv Bhakt <dhruv@cital.co>
2025-01-30 13:40:40 +0530

    Update ImageViewer to support selected image display

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,17 +2,36 @@ import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 const placeholderImage = require("@/assets/images/background-image.png");
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image");
+      return;
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={placeholderImage} />
+        <ImageViewer imgSource={selectedImage || placeholderImage} />
       </View>
 
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
-        <Button label="Use this photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" onPress={() => alert('You pressed a button.')} />
       </View>
     </View>
   );
